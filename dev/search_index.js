c62b67ee8891ac9596c22647c72808ebfc3a1567
Documenter.jl <documenter@juliadocs.github.io>
2023-02-17 07:20:19 +0000

    build based on 4f473d5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SWCForecastBase","category":"page"},{"location":"#SWCForecastBase","page":"Home","title":"SWCForecastBase","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SWCForecastBase.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SWCForecastBase]","category":"page"}]
+}
