bc87cbc0ae31e02e1767495aa92d43820b92d440
okatsn <okatsn@gmail.com>
2023-03-27 19:39:39 +0800

    New method of save; docstring of save

--- src/preparetable/save.jl
+++ src/preparetable/save.jl
@@ -1,7 +1,13 @@
-function save(PT::PrepareTable)
-    tag = randstring(6)
-    dir0 = "Tables_$tag"
+"""
+`save(PT::PrepareTable, dir0)` save `cache` in `PT` to the directory `dir0`.
 
+# Example
+```julia
+dir0 = "MyResults"
+save(PT::PrepareTable, dir0)
+```
+"""
+function save(PT::PrepareTable, dir0)
     for nm in fieldnames(Cache)
         targetdir = mkpath(joinpath(dir0, string(nm)))
         tts = getfield(PT.cache, nm)
@@ -11,7 +17,21 @@ function save(PT::PrepareTable)
     end
 end
 
-save(tts) = nothing # do nothing otherwise
+"""
+Without specifying the parent folder `dir0`,
+a folder with randomstring will be generated.
+
+# Example
+```julia
+save(PT::PrepareTable) # save results to /Tables_Fsd0w4/... for example
+```
+"""
+function save(PT::PrepareTable)
+    tag = randstring(6)
+    dir0 = "Tables_$tag"
+    save(PT, dir0)
+end
+
 _save(targetdir, k, v) = nothing
 
 function save(tts::TrainTestState, targetdir)
