90610b35d8f4cd9939d32b346cfff5843451ff57
okatsn <okatsn@gmail.com>
2023-03-27 17:28:12 +0800

    Fix test

--- test/preparetable.jl
+++ test/preparetable.jl
@@ -2,23 +2,23 @@
     using SWCDatasets
     passed = false
 
-    df = SWCDatasets.dataset("NCUWiseLab", "ARI_G2F820")
-    PT = PrepareTable(df)
-    PT = PrepareTableDefault(df)
-    PTx = PrepareTableDefault(df)
+    PT =  PrepareTableDefault(SWCDatasets.dataset("NCUWiseLab", "ARI_G2F820"))
+    PTx = PrepareTableDefault(SWCDatasets.dataset("NCUWiseLab", "ARI_G2F820"))
 
     train!(PT; train_before = DateTime(2022, 03, 21))
     test!(PT; test_after = DateTime(2022, 3, 22))
+
+    traintest!(PTx; test_after = DateTime(2022, 3, 22), train_before = DateTime(2022, 03, 21))
+
+    @test isequal(PTx.cache.train.args.t, PT.cache.train.args.t)
+    @test isequal(PTx.cache.test.args.t, PT.cache.test.args.t)
+
 
     fnm = PT.supervised_tables.T |> names |> only |> Symbol
     firstid = (PT.supervised_tables.T[!, fnm] .<= DateTime(2022,3,22)) |> findlast
     PT.supervised_tables.T[firstid, fnm] = DateTime(2099, 1,1)
     @test isequal(DateTime(2099, 1,1), PT.cache.test.args.t[1])
 
-    traintest!(PTx; test_after = DateTime(2022, 3, 22), train_before = DateTime(2022, 03, 21))
-
-    @test isequal(PTx.cache.train.args.t, PT.cache.train.args.t)
-    @test isequal(PTx.cache.train.args.t, PT.cache.train.args.t)
 
     # TODO: it might be more appropriate to have this test in TWAISWCF, since it has SWCDatasets and SWCForecastBase both involved.
 
