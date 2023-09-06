a27eb6b38bd5d6cb957bccd88f31ce6bbe2c4b8a
okatsn <okatsn@gmail.com>
2023-09-06 10:09:45 +0800

    Fix `take_last_hour`

--- src/combinegroup.jl
+++ src/combinegroup.jl
@@ -11,5 +11,8 @@ end
 `combine` the `df` which were grouped by `:hour` taking only the `last`.
 """
 function take_hour_last(df)
-    combinegroup_allcols(df, :hour, last)
+    @chain df begin
+        groupby([:year, :month, :day, :hour])
+        combine(last; renamecols=false) # last(df::DataFrame) returns the last row
+    end
 end
--- test/utilities.jl
+++ test/utilities.jl
@@ -1,5 +1,6 @@
 @testset "utilities.jl" begin
     using DataFrames
+
     df = DataFrame(a=repeat([1, 2, 3, 4], outer=[2]),
         b=repeat([2, 1], outer=[4]),
         c=1:8)
@@ -10,4 +11,25 @@
     @test df2.c[end] == 1
     @test isequal(df2.a, [2, 1])
 
+end
+
+@testset "take_last_hour" begin
+    using DataFrames, Dates
+
+    df3 = DataFrame(
+        :datetime => DateTime(2018, 2, 5, 0, 0, 0):Second(1):DateTime(2018, 2, 7, 23, 59, 59)
+    )
+    # insertcols!(df3, :x)
+    transform!(df3, :datetime => ByRow(dt -> (
+        year=Dates.year(dt),
+        month=Dates.month(dt),
+        day=Dates.day(dt),
+        hour=Dates.hour(dt),
+        minute=Dates.minute(dt),
+        second=Dates.second(dt),
+    )) => AsTable)
+
+    df3c = SWCForecastBase.take_hour_last(df3)
+    @test all(df3c.minute .== 59)
+    @test all(df3c.second .== 59)
 end
